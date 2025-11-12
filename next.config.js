65a1ef73657f4197e57819a51166d4c2d8a4f3b9
Raunak Kumar <per.singularity@gmail.com>
2025-11-13 00:45:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,21 @@
 const nextConfig = {
   output: 'standalone',
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   experimental: {
-    // Remove if not using Server Components
     serverComponentsExternalPackages: ['mongodb'],
+    appDir: true,
   },
+  dynamic: 'force-dynamic', // ⬅️ add this line
+
   webpack(config, { dev }) {
     if (dev) {
-      // Reduce CPU/memory from file watching
       config.watchOptions = {
-        poll: 2000, // check every 2 seconds
-        aggregateTimeout: 300, // wait before rebuilding
+        poll: 2000,
+        aggregateTimeout: 300,
         ignored: ['**/node_modules'],
       };
     }
     return config;
-  },
-  onDemandEntries: {
-    maxInactiveAge: 10000,
-    pagesBufferLength: 2,
   },
   async headers() {
     return [
